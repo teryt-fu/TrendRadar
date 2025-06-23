b4854ba3a314c43758db92911c2644c21b5d4195
teryt <43084784+teryt-fu@users.noreply.github.com>
2025-06-23 15:13:02 +0800

    Update main.py
    6

--- main.py
+++ main.py
@@ -2203,16 +2203,11 @@ class NewsAnalyzer:
         print(f"报告类型: {self.report_type}")
 
         ids = [
-            ("toutiao", "今日头条"),
             ("baidu", "百度热搜"),
-            ("wallstreetcn-hot", "华尔街见闻"),
-            ("thepaper", "澎湃新闻"),
             ("bilibili-hot-search", "bilibili 热搜"),
-            ("cls-hot", "财联社热门"),
             ("ifeng", "凤凰网"),
             "tieba",
             "weibo",
-            "douyin",
             "zhihu",
             "github",
             "juejin",
